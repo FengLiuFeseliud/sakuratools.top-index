afcff3a6585bc1703ab636da31af203ac5aefb4c
sakura <1718089268@qq.com>
2022-01-25 19:16:02 +0800

    fix bug

---
+++ src/components/svue/main.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="index-main" class="clearfix">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+    /* 页面盒子 */
+    #index-main {
+        min-height: 800px;
+        margin: 100px auto;
+        margin-bottom: 50px;
+        opacity: 0.8;
+        color: var(--main-font-color);
+        font-size: 18px;
+        line-height: 32px;
+    }
+
+    #index-main > .last {
+        float: left;
+        margin-right: 0px;
+    }
+</style>
---
+++ src/views/error.vue
@@ -0,0 +1,49 @@
+<template>
+    <Main>
+        <div id="error">
+            <h1>404 / 403!</h1>
+            <p>1. 没有这个地址, 请检查拼写</p>
+            <p>2. 无权访问这个地址</p>
+            <router-link to="/">返回主页</router-link>
+        </div>
+    </Main>
+</template>
+
+<script>
+    import Main from "../components/svue/main.vue"
+
+    export default {
+        components:{
+            Main
+        }
+    }
+</script>
+
+<style scoped>
+    #error {
+        padding: 20px;
+        margin: 210px auto;
+        width: 450px;
+        height: 300px;
+        opacity: 0.8;
+        background-color: var(--main-background-color);
+        color: var(--main-font-color);
+    }
+
+    #error a {
+        display: block;
+        margin: 0px auto;
+        margin-top: 120px;
+        width: 200px;
+        height: 50px;
+        background-color: var(--two-background-color);
+        color: var(--hover-font-color);
+        text-align: center;
+        line-height: 50px;
+    }
+
+    #error a:hover{
+        background-color: var(--main-background-color);
+        color: var(--two-font-color);
+    }
+</style>
